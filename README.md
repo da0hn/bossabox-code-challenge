5c5d067859299625db5c459b27b97f475bfc5608
Da0hn <gabrielhondacba@gmail.com>
2022-10-06 19:38:15 -0400

    chore: initial setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# BossaBox code challenge
+
+* Implementation of VUTTR (Very Useful Tools to Remember) using stack
+  `Java 17`, `Spring Boot 3.0` and `Neo4j Graph Database`.
+
+* The requirements are describe in [notion](https://www.notion.so/Back-end-0b2c45f1a00e4a849eefe3b1d57f23c6).
+
+
---
+++ vuttr/src/main/java/com/gabriel/vuttr/VuttrApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.vuttr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuttrApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(VuttrApplication.class, args);
+  }
+
+}
