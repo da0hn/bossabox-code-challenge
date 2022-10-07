87c99713f9d3950ef75868eeef5dbcc124acee8b
Da0hn <gabrielhondacba@gmail.com>
2022-10-07 18:51:07 -0400

    refactor: remove `TAGGED_BY` relation from `Tag` entity

--- vuttr/src/main/java/com/gabriel/vuttr/domain/entities/Tag.java
+++ vuttr/src/main/java/com/gabriel/vuttr/domain/entities/Tag.java
@@ -1,14 +1,11 @@
 package com.gabriel.vuttr.domain.entities;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.neo4j.core.schema.Node;
-import org.springframework.data.neo4j.core.schema.Relationship;
 
 import java.io.Serial;
 import java.time.LocalDateTime;
-import java.util.Set;
 
 @Getter
 @Setter
@@ -18,20 +15,15 @@ public class Tag extends NodeEntity {
   @Serial
   private static final long serialVersionUID = -6759535410416918406L;
 
-
   private String name;
-  @Relationship(type = "TAGGED_BY", direction = Relationship.Direction.INCOMING)
-  private Set<Tool> tools;
 
   public Tag(
     final Long id,
     final LocalDateTime createdAt,
-    final String name,
-    final Set<Tool> tools
+    final String name
   ) {
     super(id, createdAt);
     this.name = name;
-    this.tools = tools;
   }
 
   public Tag() {
