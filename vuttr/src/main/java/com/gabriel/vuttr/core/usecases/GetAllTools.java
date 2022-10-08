558a60a8932546c52d105cb5f8f955c6399af704
Da0hn <gabrielhondacba@gmail.com>
2022-10-07 21:00:44 -0400

    refactor: use `@AllArgsConstructor`

--- vuttr/src/main/java/com/gabriel/vuttr/core/usecases/GetAllTools.java
+++ vuttr/src/main/java/com/gabriel/vuttr/core/usecases/GetAllTools.java
@@ -3,16 +3,16 @@ package com.gabriel.vuttr.core.usecases;
 import com.gabriel.vuttr.core.ports.api.IGetAllTools;
 import com.gabriel.vuttr.core.dtos.ToolItemFilterParameter;
 import com.gabriel.vuttr.core.dtos.ToolItemResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@AllArgsConstructor
 public class GetAllTools implements IGetAllTools {
 
   private final Repository repository;
-
-  public GetAllTools(final Repository repository) {this.repository = repository;}
 
   @Override
   public List<ToolItemResponse> execute(final ToolItemFilterParameter parameter) {
