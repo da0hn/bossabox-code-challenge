9f737c9753bc58abf925c6ab8491a67285963f00
Da0hn <gabrielhondacba@gmail.com>
2022-10-08 10:17:54 -0400

    fix: enable generation of `createdAt` field

--- vuttr/src/main/java/com/gabriel/vuttr/VuttrApplication.java
+++ vuttr/src/main/java/com/gabriel/vuttr/VuttrApplication.java
@@ -2,8 +2,10 @@ package com.gabriel.vuttr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.neo4j.config.EnableNeo4jAuditing;
 
 @SpringBootApplication
+@EnableNeo4jAuditing
 public class VuttrApplication {
 
   public static void main(final String[] args) {
