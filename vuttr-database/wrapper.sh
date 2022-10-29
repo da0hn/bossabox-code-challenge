e46449eefc79975e35af45e3e4e0e0834375c96e
Da0hn <gabrielhondacba@gmail.com>
2022-10-29 18:21:25 -0400

    refactor: add better messages to communicate optional database seeding

--- vuttr-database/wrapper.sh
+++ vuttr-database/wrapper.sh
@@ -16,22 +16,24 @@ wget --quiet --tries=10 --waitretry=10 -O /dev/null http://localhost:7474
 
 HAS_DATA=$(echo $(cypher-shell --format plain "MATCH (n) RETURN count(n) as COUNT") | sed 's/[^0-9]*//g')
 
-log_info "Has data $(echo ${HAS_DATA}) nodes"
+log_info "Already has data $(echo ${HAS_DATA}) nodes"
 
 if [[ $HAS_DATA -eq 0 && -d "/cyphers" ]]; then
   log_info "Deleting all relations"
   cypher-shell --format plain "MATCH (n) DETACH DELETE n"
 
-  log_info "Wrapper: Loading cyphers from '/cyphers'"
+  log_info "Loading cyphers from '/cyphers'"
   for cipherFile in /cyphers/*.cql; do
     log_info "Running cypher ${cipherFile}"
     contents=$(cat "${cipherFile}")
     cypher-shell -a bolt://localhost:7687 "${contents}"
   done
   log_info "Finished loading all cyphers from '/cyphers'"
-fi
 
-TOTAL_CHANGES=$(cypher-shell --format plain "MATCH (n) RETURN count(n) AS count")
-log_info "Changes $(echo ${TOTAL_CHANGES} | sed -e 's/[\r\n]//g')"
+  TOTAL_CHANGES=$(cypher-shell --format plain "MATCH (n) RETURN count(n) AS count")
+  log_info "Changes $(echo ${TOTAL_CHANGES} | sed -e 's/[\r\n]//g')"
+else
+  log_info "Seeding not necessary, skipping..."
+fi
 
 fg %1
