3dd1fb7c823d0962c533a3a568d482afeb88cc9d
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-03-11 09:48:55 -0400

    refactor: make `ItemResponse<T>` an specific generic interface equivalent to `Response<Collection<T>>`

--- vuttr/src/main/java/com/gabriel/vuttr/application/commons/ItemResponse.java
+++ vuttr/src/main/java/com/gabriel/vuttr/application/commons/ItemResponse.java
@@ -2,7 +2,7 @@ package com.gabriel.vuttr.application.commons;
 
 import java.util.Collection;
 
-public interface ItemResponse<T> {
+public interface ItemResponse<T> extends Response<Collection<T>> {
 
   Collection<T> getData();
 
