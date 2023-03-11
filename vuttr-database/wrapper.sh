9b967224b60733c491011f48cc9ed64229301be6
Da0hn <gabrielhondacba@gmail.com>
2023-03-11 10:32:52 -0400

    chore: setup Neo4j database password and adapt in `wrapper.sh` script
    - add `NEO4J_USER` and `NEO4J_PASS`
    - setup user and password in `application.yml`
    - remove `user-data.cql` file

--- vuttr-database/wrapper.sh
+++ vuttr-database/wrapper.sh
@@ -11,26 +11,28 @@ set -m
 
 /docker-entrypoint.sh neo4j &
 
+log_info "USER: $NEO4J_USER PASSWORD: $NEO4J_PASS"
+
 log_info "Waiting until neo4j stats at :7474 ..."
 wget --quiet --tries=10 --waitretry=10 -O /dev/null http://localhost:7474
 
-HAS_DATA=$(echo $(cypher-shell --format plain "MATCH (n) RETURN count(n) as COUNT") | sed 's/[^0-9]*//g')
+HAS_DATA=$(echo $(cypher-shell -a bolt://localhost:7687 -u $NEO4J_USER -p $NEO4J_PASS  --format plain "MATCH (n) RETURN count(n) as COUNT") | sed 's/[^0-9]*//g')
 
 log_info "Already has data $(echo ${HAS_DATA}) nodes"
 
 if [[ $HAS_DATA -eq 0 && -d "/cyphers" ]]; then
   log_info "Deleting all relations"
-  cypher-shell --format plain "MATCH (n) DETACH DELETE n"
+  cypher-shell -a bolt://localhost:7687 -u $NEO4J_USER -p $NEO4J_PASS --format plain "MATCH (n) DETACH DELETE n"
 
   log_info "Loading cyphers from '/cyphers'"
   for cipherFile in /cyphers/*.cql; do
     log_info "Running cypher ${cipherFile}"
     contents=$(cat "${cipherFile}")
-    cypher-shell -a bolt://localhost:7687 "${contents}"
+    cypher-shell -a bolt://localhost:7687 -u $NEO4J_USER -p $NEO4J_PASS "${contents}"
   done
   log_info "Finished loading all cyphers from '/cyphers'"
 
-  TOTAL_CHANGES=$(cypher-shell --format plain "MATCH (n) RETURN count(n) AS count")
+  TOTAL_CHANGES=$(cypher-shell -a bolt://localhost:7687 -u $NEO4J_USER -p $NEO4J_PASS --format plain "MATCH (n) RETURN count(n) AS count")
   log_info "Changes $(echo ${TOTAL_CHANGES} | sed -e 's/[\r\n]//g')"
 else
   log_info "Seeding not necessary, skipping..."
