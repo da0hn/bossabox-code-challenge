4e3e51c1009f37a6c8b94de880f7ea156273714a
Da0hn <gabrielhondacba@gmail.com>
2023-04-13 23:18:06 -0400

    chore: add `tailwind` configuration

--- vuttr-react-app/src/App.tsx
+++ vuttr-react-app/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import './App.css';
+import './styles/globals.css';
 
 function App() {
     return (
-        <h1>VUTTR Application</h1>
+        <h1 className="text-3xl font-bold underline">
+            VUTTR Application
+        </h1>
     );
 }
 
