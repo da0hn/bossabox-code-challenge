3611a3ca2683d725e61feaf4acc3dadea12c3fa6
Da0hn <gabrielhondacba@gmail.com>
2023-04-14 20:40:55 -0400

    chore: configure `react-router-dom` initial page

--- vuttr-react-app/src/index.tsx
+++ vuttr-react-app/src/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import {RouterProvider} from "react-router-dom";
+import {router} from "@vuttr/routes/router";
+
+import '@vuttr/styles/globals.css';
+import "primereact/resources/themes/mira/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <App/>
+        <RouterProvider router={router}></RouterProvider>
     </React.StrictMode>
 );
 
