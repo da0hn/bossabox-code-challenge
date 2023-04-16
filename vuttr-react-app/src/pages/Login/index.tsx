dd147d6fb93d903e0d16a4eb7c851384f39b5733
Da0hn <gabrielhondacba@gmail.com>
2023-04-16 10:01:27 -0400

    feat: create login page template without behavior

--- vuttr-react-app/src/pages/Login/index.tsx
+++ vuttr-react-app/src/pages/Login/index.tsx
@@ -1,5 +1,34 @@
+import React, {FormEvent, useCallback} from "react";
+import {InputText} from "primereact/inputtext";
+import {Card} from "primereact/card";
+import {Button} from "primereact/button";
+
 export default function Login() {
+    const onFormSubmit = useCallback((event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    }, []);
+
     return (
-        <h1>Login Page</h1>
+        <main className="h-screen flex items-center justify-center">
+            <Card>
+                <div className="m-8">
+                    <form className="flex flex-col gap-8 w-full max-w-xs" onSubmit={onFormSubmit}>
+                        <div className="flex flex-col gap-1">
+                            <span className="p-float-label">
+                                <InputText id="username"/>
+                                <label htmlFor="username">Username</label>
+                            </span>
+                        </div>
+                        <div className="flex flex-col gap-1">
+                            <span className="p-float-label">
+                                <InputText id="password" type="password"/>
+                                <label htmlFor="password">Password</label>
+                            </span>
+                        </div>
+                        <Button type="submit" label="Login" raised/>
+                    </form>
+                </div>
+            </Card>
+        </main>
     );
 }
